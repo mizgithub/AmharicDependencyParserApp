897785af2f7928e43413967f0b615e238b8feee9
mizgithub <mizanu143@gmail.com>
2021-11-20 11:18:46 +0300

    'last'

--- index.py
+++ index.py
@@ -328,7 +328,7 @@ def singleSentenceParsingPage():
             htmlString+="</table>"
             # st.markdown(htmlString,unsafe_allow_html = True)
             
-            graph = graphviz.Digraph()
+            graph = graphviz.Digraph(format="png")
             
             graph.node('ROOT')
             for i in range(len(result)):
@@ -382,7 +382,7 @@ def singleSentenceParsingPage():
         st.markdown(htmlString, unsafe_allow_html = True)
         st.markdown("<hr>", unsafe_allow_html = True)
         st.subheader("Graph Representation")
-        st.graphviz_chart('''graph''')
+        st.graphviz_chart(graph)
     
         st.graphviz_chart('''
             digraph {
