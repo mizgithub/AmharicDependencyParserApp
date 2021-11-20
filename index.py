be2c7fd501169b4078bdb94d9ec0ab5c7e332230
mizgithub <mizanu143@gmail.com>
2021-11-20 10:58:26 +0300

    'last'

--- index.py
+++ index.py
@@ -383,6 +383,24 @@ def singleSentenceParsingPage():
         st.markdown("<hr>", unsafe_allow_html = True)
         st.graphviz_chart(graph)
 
+        st.graphviz_chart('''
+            digraph {
+                run -> intr
+                intr -> runbl
+                runbl -> run
+                run -> kernel
+                kernel -> zombie
+                kernel -> sleep
+                kernel -> runmem
+                sleep -> swap
+                swap -> runswap
+                runswap -> new
+                runswap -> runmem
+                new -> runmem
+                sleep -> runmem
+            }
+        ''')
+
         
 
 st.header("Amharic Dependency Parser System")
